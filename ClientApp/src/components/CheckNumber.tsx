f30068e76acb95c9230cbbdff933430dacfa9f9c
lishaowen <litony0426@gmail.com>
2024-06-05 19:03:25 +0900

    update

--- ClientApp/src/components/CheckNumber.tsx
+++ ClientApp/src/components/CheckNumber.tsx
@@ -1,6 +1,28 @@
-import { forwardRef, ComponentPropsWithoutRef } from "react";
+import { forwardRef, ComponentPropsWithoutRef, useState } from "react";
 import { cn } from "../lib";
 import { Keyboard } from "lucide-react";
+import { motion, Variants } from "framer-motion";
+
+const NumberInput = forwardRef<
+    HTMLInputElement,
+    ComponentPropsWithoutRef<"input">
+>((props, ref) => {
+    return <input ref={ref} className="number-input"></input>;
+});
+
+const InputPad = forwardRef<HTMLDivElement, ComponentPropsWithoutRef<"div">>(
+    (props, ref) => {
+        return (
+            <div ref={ref} className="number-input-pad">
+                {Array(5 * 6)
+                    .fill(1)
+                    .map(() => (
+                        <NumberInput />
+                    ))}
+            </div>
+        );
+    }
+);
 
 const Number = forwardRef<
     HTMLButtonElement,
@@ -8,6 +30,7 @@ const Number = forwardRef<
 >(({ value, className, disabled, ...props }, ref) => {
     return (
         <button
+            ref={ref}
             className={cn("number-button", className)}
             disabled={disabled ? true : false}
         >
@@ -15,36 +38,51 @@ const Number = forwardRef<
         </button>
     );
 });
+
+const MotionNumber = motion(Number);
 const NumberPad = forwardRef<HTMLDivElement, ComponentPropsWithoutRef<"div">>(
     (props, ref) => {
         return (
-            <div className="number-pad">
+            <div className="number-pad" ref={ref}>
                 {Array(37)
                     .fill(1)
                     .map((_, index) => (
-                        <Number value={index + 1} />
+                        <MotionNumber value={index + 1} />
                     ))}
             </div>
         );
     }
 );
 
+const MotionNumberPad = motion(NumberPad);
+
 const NumberPadButton = forwardRef<
     HTMLButtonElement,
     ComponentPropsWithoutRef<"button">
 >((props, ref) => {
+    const [expand, setExpand] = useState<boolean>(false);
     return (
-        <button className="w-[48px] h-[48px] rounded-full border-[2px] border-solid border-black/30 flex justify-center items-center sticky left-[100px] top-[500px]">
-            <Keyboard />
-        </button>
+        <>
+            <button
+                className="number-pad-button  rounded-full border-[2px] border-solid border-black/30 flex justify-center items-center"
+                onClick={() => {
+                    setExpand((v) => !v);
+                }}
+            >
+                <Keyboard />
+            </button>
+        </>
     );
 });
 const CheckNumber = forwardRef<HTMLDivElement, ComponentPropsWithoutRef<"div">>(
     (props, ref) => {
         return (
-            <div className="w-full min-h-[100vh] overflow-auto">
-                <NumberPadButton />
-            </div>
+            <>
+                <div className="w-full  flex flex-col items-center pt-[100px] gap-[50px]">
+                    <InputPad />
+                    <NumberPad />
+                </div>
+            </>
         );
     }
 );
